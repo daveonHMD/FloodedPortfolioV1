6e7d7a5719d0273acbb27723a620e8657e18c51e
Flooded <129466844+daveonHMD@users.noreply.github.com>
2025-02-01 13:59:43 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect, useState } from "react";
 import Head from "next/head";
-import Link from "next/link";
-import Image from "next/image";
 import NavComponent from "../components/partials/navbar";
 import config from "../config.json";
 
@@ -18,9 +16,13 @@ export default function App({ Component, pageProps }: AppProps) {
         fetch(`https://api.github.com/users/${githubUsername}`)
             .then((response) => response.json())
             .then((data) => {
-                setAvatarUrl(data.avatar_url);
-                setUsername(data.name);
-                updateFavicon(data.avatar_url);
+                if (data.avatar_url && data.name) {
+                    setAvatarUrl(data.avatar_url);
+                    setUsername(data.name);
+                    updateFavicon(data.avatar_url);
+                } else {
+                    console.error("GitHub data missing expected fields.");
+                }
             })
             .catch((error) => {
                 console.error("Error fetching GitHub avatar:", error);
@@ -28,9 +30,7 @@ export default function App({ Component, pageProps }: AppProps) {
     }, []);
 
     const updateFavicon = (url: string) => {
-        const favicon = document.querySelector(
-            'link[rel="icon"]'
-        ) as HTMLLinkElement;
+        const favicon = document.querySelector('link[rel="icon"]') as HTMLLinkElement;
         if (favicon) {
             favicon.href = url;
         } else {
@@ -44,7 +44,12 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
-                <title>{username} | Portfolio</title>
+                <title>{username || "Portfolio"} | Portfolio</title>
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9750247758090908"
+                    crossOrigin="anonymous"
+                ></script>
             </Head>
             <NavComponent />
             <Component {...pageProps} />
